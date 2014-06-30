0373d443be91e29901aeae0515c782419595a1bf
saranpol <saranpol@saranpols-Mac-mini.local>
2014-06-30 18:09:19 +0700

    more ui

--- Gambling/Gambling/GBViewController.h
+++ Gambling/Gambling/GBViewController.h
@@ -13,15 +13,19 @@
 
 @property (nonatomic, assign) NSInteger mYouHave;
 @property (nonatomic, assign) NSInteger mYouBet;
-@property (nonatomic, weak) IBOutlet UISlider *mSlider;
+@property (nonatomic, assign) NSTimeInterval mInterval;
 @property (nonatomic, weak) IBOutlet UILabel *mLabelYouHave;
 @property (nonatomic, weak) IBOutlet UILabel *mLabelYouBet;
 @property (nonatomic, weak) IBOutlet UILabel *mLabelWin;
 @property (nonatomic, weak) IBOutlet UILabel *mLabelLose;
 
 
-- (IBAction)sliderChange:(id)sender;
 - (IBAction)clickA:(id)sender;
 - (IBAction)clickB:(id)sender;
+- (IBAction)downMinus:(id)sender;
+- (IBAction)downPlus:(id)sender;
+- (IBAction)clickD2:(id)sender;
+- (IBAction)clickX2:(id)sender;
+- (IBAction)anyUp:(id)sender;
 
 @end
--- Gambling/Gambling/GBViewController.m
+++ Gambling/Gambling/GBViewController.m
@@ -13,7 +13,7 @@
 
 @synthesize mYouHave;
 @synthesize mYouBet;
-@synthesize mSlider;
+@synthesize mInterval;
 @synthesize mLabelYouHave;
 @synthesize mLabelYouBet;
 @synthesize mLabelWin;
@@ -24,7 +24,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    self.mYouHave = 10000;
+    self.mYouHave = 100000;
+    self.mYouBet = 100;
 
     [self updateYouHave];
     [self updateYouBet];
@@ -41,32 +42,37 @@
 }
 
 - (void)updateYouBet {
-    self.mYouBet = fabs(mSlider.value * mYouHave);
+    //self.mYouBet = fabs(mSlider.value * mYouHave);
+    if(mYouBet >= mYouHave)
+        self.mYouBet = mYouHave;
     [mLabelYouBet setText:[NSString stringWithFormat:@"$%ld", (long)mYouBet]];
 }
 
 
 
-
-- (IBAction)sliderChange:(id)sender {
-    [self updateYouBet];
-}
-
 - (void)randomWin {
     if(arc4random()%2){
         // win
         self.mYouHave += mYouBet;
+        [mLabelWin setHidden:NO];
+        [mLabelLose setHidden:YES];
         [mLabelWin setAlpha:1];
         [UIView animateWithDuration:2.0 animations:^{
             [mLabelWin setAlpha:0];
         }];
+//        self.mYouBet = 100;
     }else{
         // lose
         self.mYouHave -= mYouBet;
+        [mLabelWin setHidden:YES];
+        [mLabelLose setHidden:NO];
         [mLabelLose setAlpha:1];
         [UIView animateWithDuration:2.0 animations:^{
             [mLabelLose setAlpha:0];
         }];
+//        self.mYouBet *= 2;
+//        if(mYouBet >= mYouHave)
+//            self.mYouBet = mYouHave;
     }
     [self updateYouHave];
     [self updateYouBet];
@@ -79,6 +85,80 @@
 - (IBAction)clickB:(id)sender {
     [self randomWin];
 }
+
+- (void)minusBet {
+    if(mInterval >= 0.1)
+        self.mYouBet--;
+    else
+        self.mYouBet -= 1.0/mInterval;
+    if(mYouBet <= 0)
+        self.mYouBet = 0;
+    [self updateYouBet];
+    [self performSelector:@selector(minusBet) withObject:nil afterDelay:mInterval];
+    self.mInterval /= 1.2;
+    if(mInterval <= 0.005)
+        mInterval = 0.005;
+}
+
+- (void)plusBet {
+    if(mInterval >= 0.1)
+        self.mYouBet++;
+    else
+        self.mYouBet += 1.0/mInterval;
+    if(mYouBet >= mYouHave)
+        self.mYouBet = mYouHave;
+    [self updateYouBet];
+    [self performSelector:@selector(plusBet) withObject:nil afterDelay:mInterval];
+    self.mInterval /= 1.2;
+    if(mInterval <= 0.005)
+        mInterval = 0.005;
+}
+
+
+- (IBAction)downMinus:(id)sender {
+    self.mInterval = 0.2;
+    [self minusBet];
+}
+
+
+- (IBAction)downPlus:(id)sender {
+    self.mInterval = 0.2;
+    [self plusBet];
+}
+
+- (void)d2 {
+    self.mYouBet /= 2.0;
+    if(mYouBet <= 0)
+        self.mYouBet = 0;
+    if(mYouBet >= mYouHave)
+        self.mYouBet = mYouHave;
+    [self updateYouBet];
+    [self performSelector:@selector(d2) withObject:nil afterDelay:mInterval];
+}
+
+- (void)x2 {
+    self.mYouBet *= 2.0;
+    if(mYouBet >= mYouHave)
+        self.mYouBet = mYouHave;
+    [self updateYouBet];
+    [self performSelector:@selector(x2) withObject:nil afterDelay:mInterval];
+}
+
+
+- (IBAction)clickD2:(id)sender {
+    self.mInterval = 0.2;
+    [self d2];
+}
+
+- (IBAction)clickX2:(id)sender {
+    self.mInterval = 0.2;
+    [self x2];
+}
+
+- (IBAction)anyUp:(id)sender {
+    [NSObject cancelPreviousPerformRequestsWithTarget:self];
+}
+
 
 
 @end
